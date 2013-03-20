cfe69f2ffa3e619ca9b7e506c8ae3694f02fb207
Tati Carvalho <tatigmc@yahoo.com.br>
2013-03-20 11:57:16 -0300

    Arquivos iniciais. Adiiconei principais gems usadas na demo no arquivo Gemfile

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'sqlite3'
+
+
+group :assets do
+  gem "therubyracer", :platforms => :ruby
+  gem "uglifier"
+end
+
+gem 'jquery-rails'
+gem 'devise'
+gem 'koala'
+gem "less-rails"
+gem 'twitter-bootstrap-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookWithCoalaDemo::Application.config.secret_token = 'd9e2384622e3e8d5320923c73da5015d3d7ef21a02e114931866dcd051262ca02cb52c244f0c410649ffb94fd1ec8739859e2b5dde13bbd7b2e9d9999afcbfd6'
